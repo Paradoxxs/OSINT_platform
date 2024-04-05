116ec16cc0c55c631772e2ecf334f1c53e731264
Paradoxxs <Aston.dk@gmail.com>
2024-04-05 08:54:54 +0200

    added auto import bookmarks

---
+++ firefox/preferences/firefox.js
@@ -0,0 +1,10 @@
+pref("datareporting.policy.firstRunURL", "");
+pref("datareporting.policy.dataSubmissionEnabled", false);
+pref("datareporting.healthreport.service.enabled", false);
+pref("datareporting.healthreport.uploadEnabled", false);
+pref("trailhead.firstrun.branches", "nofirstrun-empty");
+pref("browser.aboutwelcome.enabled", false);
+pref("browser.bookmarks.file", "/bookmarks/bookmarks_1_17_24.html");
+pref("browser.places.importBookmarksHTML", true);
+pref("browser.bookmarks.addedImportButton", false);
+pref("browser.toolbars.bookmarks.visibility","always")
